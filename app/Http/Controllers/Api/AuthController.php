615ade2bd45d02359d16e591e36a515923e168ca
Vito <lolozol80xd@gmail.com>
2024-02-16 10:39:40 +0100

    update

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,8 +4,22 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Api\ResponseController;
+use Illuminate\Support\Facades\Auth;
+use App\models\User;
+use App\Http\Requests\UserRegisterChecker;
 
-class AuthController extends Controller
+class AuthController extends ResponseController
 {
-    //
+    public function register(){
+        
+    }
+
+    public function login(){
+
+    }
+
+    public function logout(){
+
+    }
 }
---
+++ app/Http/Requests/UserRegisterChecker.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+
+class UserRegisterChecker extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            "name"=>"required|max:20",
+            "email"=>"required|email",
+            "password"=>"required|min:6",
+            "confirm_password"=>"required|same:password"
+        ];
+    }
+    public function messages(){
+        return[
+            "name.required"=>"Név kötelező",
+            "name.max"=>"Név túl hosszú",
+            "email.required"=>"Email kötelező",
+            "email.email"=>"Valid email kötelező",
+            "password.required"=>"Jelszó kötelező",
+            "password.min"=>"Jelszó túl rövid",
+            "confirm_password.required"=>"Kötelező a jelszó megerősítés",
+            "confirm_password.same"=>"Jelszók nem egyeznek"
+
+        ];
+    }
+    public function failedValidation(Validator $validator){
+        throw new HttpResponseException(response()->json([
+            "success"=>false,
+            "message"=>"Adatbeviteli hiba",
+            "data"=>$validator->errors()
+        ]));
+    }
+}
